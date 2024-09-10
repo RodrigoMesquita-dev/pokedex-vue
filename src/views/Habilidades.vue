caed6d10b4e47ee2d343f8af1cf312e11f1a7ad3
rodrigomesquita-dev <rodrigomesquita9232@gmail.com>
2024-09-10 09:13:00 -0400

    chore: removing habilities, and adding animations

--- src/views/Habilidades.vue
+++ src/views/Habilidades.vue
@@ -4,12 +4,20 @@
       <div v-else >
         <table class="table text-white">
             <tbody>
+              <transition-group name="lista">
                 <tr v-for="(h, indice) in pokemon.habilidades" :key="indice">
                     <td>{{ h }}</td>
                     <td class="d-flex justify-content-end">
-                        <button type="button" class="btn btn-danger btn-sm">x</button>
+                        <button
+                          type="button"
+                          class="btn btn-danger btn-sm"
+                          @click="$emit('removerHabilidade', indice)"
+                        >
+                          x
+                        </button>
                     </td>
                 </tr>
+              </transition-group>
             </tbody>
         </table>
         <input 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -63,7 +63,12 @@
           <div class="detalhes">
             <!-- exibe dados de acordo com o menu de navegação -->
             
-              <router-view v-slot="{ Component }" :pokemon="pokemon" @adicionarHabilidade="adicionarHabilidade" >
+              <router-view 
+                v-slot="{ Component }"
+                :pokemon="pokemon"
+                @adicionarHabilidade="adicionarHabilidade"
+                @removerHabilidade="removerHabilidade"
+              >
                 <transition
                   enter-active-class="animate__animated animate__zoomInDown"
                 >
@@ -173,6 +178,11 @@ export default {
     },
     adicionarHabilidade(habilidade) {
       this.pokemon.habilidades.push(habilidade);
+    },
+    removerHabilidade(indice) {
+      if(this.pokemon.habilidades[indice]){
+        this.pokemon.habilidades.splice(indice, 1)
+      }
     }
   }
 }
